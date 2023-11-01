167c9d09a9e65eb0510e06578a63ea96867236bc
Leonardo de Oliveira Ramos <148880946+LE0N4RDOR4M0S@users.noreply.github.com>
2023-11-01 19:25:22 -0400

    1.0

--- GestaoVeiculos/src/main/java/com/example/demo/GestaoVeiculosApplication.java
+++ GestaoVeiculos/src/main/java/com/example/demo/GestaoVeiculosApplication.java
@@ -1,7 +1,7 @@
 /**
  *Projeto desenvolvido por Leonardo de Oliveira Ramos
  *linkedin: https://www.linkedin.com/in/leonardo-de-oliveira-ramos-690318270?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B2wAbJ3PSTVqat3ebsdPRkA%3D%3D
- *Projeto de Conclusão de Curso FIC_DEV programador de Sistemas em JAVA
+ *Projeto de Conclusão de Curso FIC_DEV programador de Sistemas na linguagem JAVA
  *Sistema de Gestão de Veículos Escolares
  */
 
